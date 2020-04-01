f0d30cd70ecf2f2b79315dd8560751095bc8d45f
Vizzyy <barnabas.vizy@gmail.com>
2020-03-31 22:24:48 -0400

    Web server

--- app.py
+++ app.py
@@ -121,6 +121,16 @@ def home(section):
     lock.release()
     return "<h1>Indoor Lights!</h1><p>"+str(section)+"</p>"
 
+@app.route('/custom', methods=['GET'])
+def custom():
+    colorValue = request.args.get('colorValue').lstrip("%23")
+    rgb = tuple(int(colorValue[i:i+2], 16) for i in (0, 2, 4))
+    print('RGB =', rgb)
+    lock.acquire()
+    threading.Thread(target=colorInstant(strip, Color(rgb[0], rgp[1], rgp[2]))).start()
+    lock.release()
+    return "200"
+
 @app.route('/static/<string:page_name>/')
 def render_static(page_name):
     return flask.render_template('%s.html' % page_name)
