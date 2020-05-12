ea2d204537dccdfcb8f572f9660c15dc14e6e1d5
Vizzyy <barnabas.vizy@gmail.com>
2020-05-12 15:42:32 -0400

    split into indoors branch

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import os
 import subprocess
 from flask import *
 from config import *
-# from systemd import journal
 import logging
 
 LIGHTS_HOME = get_home()
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+bp = Blueprint('application_context', __name__, template_folder='templates')
+
 logger = logging.getLogger('Lights')
-# journald_handler = journal.JournalHandler()
-# logger.addHandler(journald_handler)
 p = None
 
 
-@app.route('/arrange/<section>', methods=['GET'])
+@bp.route('/arrange/<section>', methods=['GET'])
 def home(section):
     global p
     if p is not None:
@@ -23,25 +23,28 @@ def home(section):
     return "<h1>Indoor Lights!</h1><p>" + str(section) + "</p>"
 
 
-@app.route('/custom', methods=['GET'])
+@bp.route('/custom', methods=['GET'])
 def custom():
-    colorValue = request.args.get('colorValue').lstrip("#")
-    rgb = tuple(int(colorValue[i:i + 2], 16) for i in (0, 2, 4))
-    logger.info('RGB =', rgb)
-
+    try:
+        colorValue = request.args.get('colorValue').lstrip("#")
+        rgb = tuple(int(colorValue[i:i + 2], 16) for i in (0, 2, 4))
+        logger.info('RGB =', rgb)
+    except Exception:
+        print("oops")
     return "200"
-
-
-@app.route('/static/<string:page_name>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
 
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+
+@bp.route('/')
+def render_home():
+    return render_template('home.html', context=get_context())
+
+
+app.register_blueprint(bp, url_prefix=get_context())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
