69c3ff89785c3c9e50f56e14539479aa992febd2
Vizzyy <barnabas.vizy@gmail.com>
2020-07-17 17:44:23 -0400

    Distributed system

---
+++ remote.py
@@ -0,0 +1,22 @@
+from neopixel import *
+from config import *
+import rpyc
+
+
+class PixelStrip(rpyc.Service):
+    LED_COUNT = get_led_count()
+    LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
+    LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+    LED_DMA = 10  # DMA channel to use for generating signal (try 10)
+    LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+    LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
+    LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
+    exposed_strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    exposed_strip.begin()
+
+
+if __name__ == "__main__":
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(PixelStrip, port=REMOTE_PORT)
+    print(f"Opened socket on port: {REMOTE_PORT}")
+    t.start()
