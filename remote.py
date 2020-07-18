7b47098a1d3bc8c82d23403500878b91b1a469ac
Vizzyy <barnabas.vizy@gmail.com>
2020-07-17 20:48:39 -0400

    Distributed system

--- remote.py
+++ remote.py
@@ -112,5 +112,5 @@ class PixelStrip(rpyc.Service):
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
     t = ThreadedServer(PixelStrip, port=REMOTE_PORT)
-    print("Opened socket on port: "+REMOTE_PORT)
+    print("Opened socket on port: "+str(REMOTE_PORT))
     t.start()
