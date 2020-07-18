f9ce431e8e971cd8f581e2b8ef38bcad2fdba34f
Vizzyy <barnabas.vizy@gmail.com>
2020-07-17 21:08:49 -0400

    Distributed system

--- remote.py
+++ remote.py
@@ -99,6 +99,7 @@ class PixelStrip(rpyc.Service):
         for j in range(256 * iterations):
             for i in range(self.exposed_strip.numPixels()):
                 self.exposed_strip.setPixelColor(i, self.wheel((int(i * 256 / self.exposed_strip.numPixels()) + j) & 255))
+                print(self.exposed_exit)
                 if self.exposed_exit:
                     return
             self.exposed_strip.show()
