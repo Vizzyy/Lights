4fdd73c72df69d1fa3f8fc23a09e779aee9218a7
Vizzyy <barnabas.vizy@gmail.com>
2020-10-21 23:46:58 -0400

    twilight_wheel2

--- child_process.py
+++ child_process.py
@@ -129,14 +129,14 @@ def twilight_wheel(pos):
 
 
 def twilight_wheel2(pos):
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 255)
-    elif pos < 170:
+    if pos <= 85:
+        return Color(pos * 3, 255 - pos * 3, 245)
+    elif pos <= 170:
         pos -= 85
-        return Color(255 - pos * 3, 0, 255)
+        return Color(255 - pos * 3, 0, 245)
     else:
         pos -= 170
-        return Color(0, pos * 3, 255)
+        return Color(0, pos * 3, 245)
 
 
 def rainbow(strip, wait_ms=20, iterations=10000):
