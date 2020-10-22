749c420d69c469de3c8801d1e5d907914320f3d8
Your Name <you@example.com>
2020-10-22 11:48:59 -0400

    Change library

--- child_process.py
+++ child_process.py
@@ -1,6 +1,6 @@
 import math
 
-from neopixel import *
+from rpi_ws281x import PixelStrip, Color
 import sys
 import time
 from config import *
@@ -13,8 +13,7 @@ LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-# LED_COUNT = 150
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
 
 
